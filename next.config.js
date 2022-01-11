1f14d4c4e2811fe76f32195fbcd4e0ec594301f1
Joseph bayad <jobayad66@gmail.com>
2022-01-11 21:58:18 +0100

    setting ssr to false

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-
+const dynamic = require('next/dynamic');
+const ModalVideo = dynamic(() => import('react-modal-video'), { ssr: false });
 module.exports = withPlugins([optimizedImages], { target: 'serverless' });
