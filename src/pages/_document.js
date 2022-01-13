8395e27667adc54107fc94df4114f06438c8f804
Joseph bayad <jobayad66@gmail.com>
2022-01-13 16:21:27 +0100

    removing paypal sdk

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,6 @@ class CustomDocument extends Document {
           <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
           <meta name='msapplication-TileColor' content='#da532c' />
           <meta name='theme-color' content='#ffffff' />
-          <script src='https://www.paypal.com/sdk/js?client-id=AH8q3dGK2f2vLZVgbRfLTjQPySe2yRaJHs_IVfPv9YYtY_t005NkltrOXz_JZOmjuCKX-na-zFlO7KSM'></script>
         </Head>
         <body>
           <Main />
