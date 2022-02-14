e10fd561621f19bbe621f5a8578eb8a647974d6b
Joseph bayad <jobayad66@gmail.com>
2022-02-14 18:42:14 +0100

    about me

--- src/sections/about.js
+++ src/sections/about.js
@@ -3,14 +3,21 @@ import { jsx, Container, Box, Image } from 'theme-ui';
 import TextFeature from 'components/text-feature';
 
 import aboutThumb from 'assets/about.jpg';
+import teamThumb from 'assets/team1.jpg';
 
 const data = {
-  subTitle: 'About Us',
-  title: 'AFRICAN BUSINESS DEVELOPMENT',
-  description:
-    'is a consulting firm specializing in strategy and influence. We assist decision-makers in governments, public institutions, businesses, foundations and NGOs and opinion leaders. Our founders came together to offer a new service based on a combination of financial and political skills.The scope and diversity of our experience enables us to provide strategic guidance combining complementary expertise. Thus, we enable our clients to understand complex situations and provide guidance and decisive impetus to their projects. An exceptional and very effective network of contacts in larger firms , and governments in Morocco and Africa.  The utmost discretion is assured.',
-  btnName: 'Get Started',
-  btnURL: 'pricing',
+  company: {
+    subTitle: 'About Us',
+    title: 'AFRICAN BUSINESS DEVELOPMENT',
+    description:
+      'is a consulting firm specializing in strategy and influence. We assist decision-makers in governments, public institutions, businesses, foundations and NGOs and opinion leaders. Our founders came together to offer a new service based on a combination of financial and political skills.The scope and diversity of our experience enables us to provide strategic guidance combining complementary expertise. Thus, we enable our clients to understand complex situations and provide guidance and decisive impetus to their projects. An exceptional and very effective network of contacts in larger firms , and governments in Morocco and Africa.  The utmost discretion is assured.',
+  },
+
+  me: {
+    title: 'HASSAN M. LAHLOU',
+    description:
+      '30 years experience in the business world in Morocco and abroad . High level of expertise in consulting and strategic planning . It has an extensive network and strong credibility with key public and private stakeholders active in Africa . It gradually develops a platform for high performance board that can support the players on the continent.Expert in cooperation and partnership for development and project management as well as human relations. He has a recognized experience in management of all types of crisis.',
+  },
 };
 
 export default function About() {
@@ -19,13 +26,25 @@ export default function About() {
       <Container sx={styles.containerBox}>
         <Box sx={styles.contentBox}>
           <TextFeature
-            subTitle={data.subTitle}
-            title={data.title}
-            description={data.description}
+            subTitle={data.company.subTitle}
+            title={data.company.title}
+            description={data.company.description}
           />
         </Box>
         <Box sx={styles.thumbnail}>
           <Image src={aboutThumb} alt='Thumbnail' />
+        </Box>
+      </Container>
+      <Container sx={styles.containerBox}>
+        <Box sx={styles.thumbnail}>
+          <Image src={teamThumb} alt='Thumbnail' />
+        </Box>
+        <Box sx={styles.contentBox}>
+          <TextFeature
+            subTitle={data.me.subTitle}
+            title={data.me.title}
+            description={data.me.description}
+          />
         </Box>
       </Container>
     </section>
