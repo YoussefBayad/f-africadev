05c0fdad70fc155e248eddadcc0184b60c96ad9a
Joseph bayad <jobayad66@gmail.com>
2022-02-14 22:57:35 +0100

    contact section

---
+++ src/sections/contact.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Container, Box } from 'theme-ui';
+import contact from 'assets/contact.svg';
+import ContactForm from '../components/contactForm';
+
+export const Contact = () => (
+  <section id='contact'>
+    <Container sx={styles.wrapper}>
+      <Box sx={styles.details}>
+        <ContactForm />
+      </Box>
+      <Box sx={styles.thumbnail}>
+        <img
+          src={contact}
+          alt='I’m John and I’m a Backend & Devops engineer!'
+        />
+      </Box>
+    </Container>
+  </section>
+);
+
+const styles = {
+  wrapper: {
+    padding: '4rem 0',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+
+    '@media (max-width: 960px) ': {
+      flexDirection: 'column',
+    },
+  },
+  details: {
+    flex: '1',
+    pr: '2rem',
+
+    ' @media (max-width: 960px)': {
+      pr: 0,
+      width: '100%',
+      order: '1',
+    },
+
+    h1: {
+      marginBottom: '2rem',
+      fontSize: '26pt',
+      color: '#212121',
+    },
+
+    p: {
+      marginBottom: '2.5rem',
+      fontSize: '20pt',
+      fontWeight: 'normal',
+      lineHeight: '1.3',
+      color: '#707070',
+    },
+  },
+  thumbnail: {
+    flex: '1',
+
+    '@media (max-width: 960px) ': {
+      width: '100%',
+      marginBottom: '2rem',
+    },
+
+    img: {
+      width: '100%',
+    },
+  },
+};
