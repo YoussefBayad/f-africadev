0e354fa589e279a74eeef24b6034fd27ddbd33e0
YoussefBayad <jobayad66@gmail.com>
2022-02-17 17:07:10 +0100

    link to pdf

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,36 +1,40 @@
 /** @jsx jsx */
-import { jsx, Container, Flex, Button } from 'theme-ui';
-import { keyframes } from '@emotion/core';
-import { Link } from 'react-scroll';
-import Logo from 'components/logo';
-import LogoDark from 'assets/logo.png';
-import MobileDrawer from './mobile-drawer';
-import menuItems from './header.data';
+import { jsx, Container, Flex, Button } from "theme-ui";
+import { keyframes } from "@emotion/core";
+import { Link } from "react-scroll";
+import Logo from "components/logo";
+import LogoDark from "assets/logo.png";
+import MobileDrawer from "./mobile-drawer";
+import menuItems from "./header.data";
 
 export default function Header({ className }) {
   return (
-    <header sx={styles.header} className={className} id='headers'>
+    <header sx={styles.header} className={className} id="headers">
       <Container sx={styles.container}>
         <Logo src={LogoDark} />
-        <Flex as='nav' sx={styles.nav}>
+        <Flex as="nav" sx={styles.nav}>
           {menuItems.map((menuItem, i) => (
             <Link
-              activeClass='active'
+              activeClass="active"
               to={menuItem.path}
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              key={i}>
+              key={i}
+            >
               {menuItem.label}
             </Link>
           ))}
         </Flex>
-
-        <Button className='donate__btn' variant='secondary'>
-          Covid 19 Products
-        </Button>
-
+        <a
+          target="_blank"
+          href="http://afrikadev.com/wp-content/uploads/2020/06/Protection-products-eng-version-1-1.pdf"
+        >
+          <Button className="donate__btn" variant="secondary">
+            Covid 19 Products
+          </Button>
+        </a>
         <MobileDrawer />
       </Container>
     </header>
@@ -52,61 +56,68 @@ const positionAnim = keyframes`
 
 const styles = {
   header: {
-    color: 'text',
-    fontWeight: 'body',
+    color: "text",
+    fontWeight: "body",
     py: 4,
-    width: '100%',
-    position: 'absolute',
+    width: "100%",
+    position: "absolute",
     top: 0,
     left: 0,
-    backgroundColor: 'background',
-    color: '#000000',
-    transition: 'all 0.4s ease',
+    backgroundColor: "background",
+    color: "#000000",
+    transition: "all 0.4s ease",
     animation: `${positionAnim} 0.4s ease`,
-    '.donate__btn': {
+    ".donate__btn": {
       flexShrink: 0,
       mr: [15, 20, null, null, 0],
-      ml: ['auto', null, null, null, 0],
-      display: 'none',
-      '@media screen and (min-width: 1024px)': {
-        display: 'block',
+      ml: ["auto", null, null, null, 0],
+      display: "none",
+      "@media screen and (min-width: 1024px)": {
+        display: "block",
       },
     },
-    '&.sticky': {
-      position: 'fixed',
-      backgroundColor: 'background',
-      color: '#000000',
-      boxShadow: '0 1px 2px rgba(0, 0, 0, 0.06)',
+    a: {
+      textDecoration: "none",
+      color: "accent",
+      "&:hover": {
+        color: "white",
+      },
+    },
+    "&.sticky": {
+      position: "fixed",
+      backgroundColor: "background",
+      color: "#000000",
+      boxShadow: "0 1px 2px rgba(0, 0, 0, 0.06)",
       py: 3,
-      'nev > a': {
-        color: 'text',
+      "nev > a": {
+        color: "text",
       },
     },
   },
   container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   nav: {
-    mx: 'auto',
-    display: 'none',
-    '@media screen and (min-width: 1024px)': {
-      display: 'block',
+    mx: "auto",
+    display: "none",
+    "@media screen and (min-width: 1024px)": {
+      display: "block",
     },
 
     a: {
       fontSize: 2,
-      fontWeight: 'body',
+      fontWeight: "body",
       px: 5,
-      cursor: 'pointer',
-      lineHeight: '1.2',
-      transition: 'all 0.15s',
-      '&:hover': {
-        color: 'primary',
+      cursor: "pointer",
+      lineHeight: "1.2",
+      transition: "all 0.15s",
+      "&:hover": {
+        color: "primary",
       },
-      '&.active': {
-        color: 'primary',
+      "&.active": {
+        color: "primary",
       },
     },
   },
--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,36 +1,41 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Button } from 'theme-ui';
-import BannerImg from 'assets/banner-thumb.jpg';
+import { jsx } from "theme-ui";
+import { Button } from "theme-ui";
+import BannerImg from "assets/banner-thumb.jpg";
 
 export default function Banner() {
   return (
-    <section sx={styles.banner} id='home'>
-      <Button variant='primary'>Covid 19 Products</Button>
+    <section sx={styles.banner} id="home">
+      <a
+        target="_blank"
+        href="http://afrikadev.com/wp-content/uploads/2020/06/Protection-products-eng-version-1-1.pdf"
+      >
+        <Button variant="primary">Covid 19 Products</Button>
+      </a>
     </section>
   );
 }
 
 const styles = {
   banner: {
-    pt: ' 85px',
-    height: '500px',
-    overflow: ['hidden', 'initial', null, 'hidden'],
+    pt: " 85px",
+    height: "500px",
+    overflow: ["hidden", "initial", null, "hidden"],
     backgroundImage: `url(${BannerImg})`,
     backgroundRepeat: `no-repeat`,
-    backgroundPosition: 'center center',
-    backgroundSize: 'cover',
-    backgroundFill: 'cover',
-    pb: '20px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'end',
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+    backgroundFill: "cover",
+    pb: "20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "end",
 
     button: {
       background:
-        'linear-gradient(rgb(255, 149, 0) 0%, rgb(255, 94, 58) 100%) rgb(0, 122, 255)',
-      '&:hover': {
-        boxShadow: 'rgb(255, 149, 0) 0px 12px 24px -10px',
+        "linear-gradient(rgb(255, 149, 0) 0%, rgb(255, 94, 58) 100%) rgb(0, 122, 255)",
+      "&:hover": {
+        boxShadow: "rgb(255, 149, 0) 0px 12px 24px -10px",
       },
     },
   },
